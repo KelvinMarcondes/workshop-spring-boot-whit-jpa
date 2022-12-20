57928b02f1fcba6aa889ae06d0c965d2f6f43d62
Kelvin Marcondes <kelvindesouza@hotmail.com>
2022-12-20 00:35:28 -0300

    created project

---
+++ src/main/java/com/curso/workshopSpringBoot3jpa/WorkshopSpringBoot3JpaApplication.java
@@ -0,0 +1,13 @@
+package com.curso.workshopSpringBoot3jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBoot3JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBoot3JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/workshopSpringBoot3jpa/WorkshopSpringBoot3JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.workshopSpringBoot3jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBoot3JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
