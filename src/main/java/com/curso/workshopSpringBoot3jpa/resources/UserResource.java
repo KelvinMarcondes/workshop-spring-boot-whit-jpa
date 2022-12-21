6a0ee5ba0fa47b2edc146365203d03762196418d
Kelvin Marcondes <kelvindesouza@hotmail.com>
2022-12-21 09:55:02 -0300

    User entity e resource

---
+++ src/main/java/com/curso/workshopSpringBoot3jpa/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.curso.workshopSpringBoot3jpa.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.workshopSpringBoot3jpa.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "maria", "maria@gmail.com", "999999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
