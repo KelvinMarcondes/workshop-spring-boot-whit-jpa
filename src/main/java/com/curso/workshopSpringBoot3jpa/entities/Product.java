34d64833440d7c1b55dfc517e4629386f1254376
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-01-04 01:55:03 -0300

    Associação muitos-para-muitos com JoinTable

--- src/main/java/com/curso/workshopSpringBoot3jpa/entities/Product.java
+++ src/main/java/com/curso/workshopSpringBoot3jpa/entities/Product.java
@@ -5,20 +5,22 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
-
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -26,12 +28,17 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@JsonIgnoreProperties("products")
+	@ManyToMany
+	@JoinTable(
+	        name = "tb_product_category",
+	        joinColumns = @JoinColumn(name = "product_id"),
+	        inverseJoinColumns = @JoinColumn(name = "category_id")
+	        )
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
-		super();
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
